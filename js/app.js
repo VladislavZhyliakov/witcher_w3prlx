bf121a603126cb68728f2f62a209efc478fdf45e
Vladyslav Zhyliakov <vladislav.zhl19@gmail.com>
2024-02-04 00:05:26 +0200

    V 0.1. Created website with parallax effect

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper(".slider", {
+  //   direction: "vertical",
+  speed: 2400,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 1.5,
+  },
+  spaceBetween: 18,
+  parallax: true,
+  //   freeMode: true,
+});
